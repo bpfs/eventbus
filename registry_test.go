ddd59429595caab7fb655dfaf644fdd06cc9fb64
BPFS <1435293080@qq.com>
2023-12-18 15:52:50 +0800

    完善测试方法

--- registry_test.go
+++ registry_test.go
@@ -9,26 +9,49 @@ func TestEventRegistry(t *testing.T) {
 	// 创建事件注册器
 	registry := NewEventRegistry()
 
-	// 注册切片上传事件总线
-	registry.RegisterEvent("file:SliceUpload", New())
+	// 注册示例A的事件总线
+	registry.RegisterEvent("example:A", New())
 
-	// 注册切片转发事件总线
-	registry.RegisterEvent("file:SliceTransfer", New())
+	// 注册示例B的事件总线
+	registry.RegisterEvent("example:B", New())
 
 	// 获取事件总线
-	sliceUploadBus := registry.GetEventBus("file:SliceUpload")
-	if sliceUploadBus == nil {
-		t.Error("Failed to get the slice upload event bus.")
+	Example_A_Bus := registry.GetEventBus("example:A")
+	if Example_A_Bus == nil {
+		t.Error("无法获取示例A的事件总线")
 	} else {
 		// 使用事件总线...
-		fmt.Println("Slice upload event bus registered.")
+		fmt.Println("已注册 示例A 的事件总线")
 	}
 
-	sliceTransferBus := registry.GetEventBus("sliceTransfer")
-	if sliceTransferBus == nil {
-		t.Error("无法获取切片传输事件总线。")
+	// 注册
+	Example_A_Bus.Subscribe("example:A", func(
+		a string, // "example:A" 需要的请求参数
+	) error {
+		fmt.Printf("打印example:A事件参数:\t%s\n\n",a)
+		return nil
+	})
+
+
+	Example_A_Bus.Publish("example:A", "参数a")
+
+	///////////////////////////////////////////
+
+	Example_B_Bus := registry.GetEventBus("example:B")
+	if Example_B_Bus == nil {
+		t.Error("无法获取示例B的事件总线。")
 	} else {
 		// 使用事件总线...
-		fmt.Println("Slice transfer event bus registered.")
+		fmt.Println("已注册 示例B 的事件总线")
 	}
+
+	// 注册
+	Example_B_Bus.Subscribe("example:B", func(
+		b string, // "example:B" 需要的请求参数
+	) error {
+		fmt.Printf("打印example:B事件参数:\t%s\n\n",b)
+		return nil
+	})
+
+	Example_B_Bus.Publish("example:B", "参数b")
 }
